1889dec4bf305bb496c8f103c7875871eb5ca94f
Rylan Meilutis <rylan.meilutis@gmail.com>
2024-10-17 22:43:36 -0400

    maybe better

--- ci_build.sh
+++ ci_build.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/bash
 
-python3 -m venv venv --system-site-packages && source venv/bin/activate
 tox
 TWINE_PASSWORD=${CI_JOB_TOKEN} TWINE_USERNAME=gitlab-ci-token python -m twine upload --repository-url "${CI_API_V4_URL}"/projects/"${CI_PROJECT_ID}"/packages/pypi target/wheels/*
 api_token=$PYPI_TOKEN
