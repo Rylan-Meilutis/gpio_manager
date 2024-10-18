28c00306b1be5c5f7e98fb62e326716477ad86d6
Rylan Meilutis <rylan.meilutis@gmail.com>
2024-10-18 00:13:47 -0400

    fix the gitlab upload command

--- ci_build.sh
+++ ci_build.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/bash
 
 tox
-TWINE_PASSWORD=${CI_JOB_TOKEN} TWINE_USERNAME=gitlab-ci-token python -m twine upload --repository-url "${CI_API_V4_URL}"/projects/"${CI_PROJECT_ID}"/packages/pypi target/wheels/*
+TWINE_PASSWORD=${CI_JOB_TOKEN} TWINE_USERNAME=gitlab-ci-token python3 -m twine upload --repository-url "${CI_API_V4_URL}"/projects/"${CI_PROJECT_ID}"/packages/pypi target/wheels/*
 api_token=$PYPI_TOKEN
 twine upload -u __token__ -p "${api_token}" target/wheels/*
 
