2167136e4eb98ade8b9526638bd28f07a626b0c0
Rylan Meilutis <rylan.meilutis@gmail.com>
2024-11-03 19:56:45 -0500

    fix the imports

--- pyi_stubs/common.pyi
+++ pyi_stubs/common.pyi
@@ -1,3 +1,5 @@
+from typing import Optional, Tuple, Callable
+
 class InternPullResistorState:
     """Enum representing the GPIO pin state types for input pins."""
     PULLUP: 'InternPullResistorState'
