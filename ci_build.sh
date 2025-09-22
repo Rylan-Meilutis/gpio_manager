e8ab4a6bb8b0b8af495eb55eb3609a88bbb0fe91
Rylan Meilutis <rylan.meilutis@gmail.com>
2025-09-22 10:47:17 -0400

    fix the build process for ci builds

--- ci_build.sh
+++ ci_build.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 apt-get update && apt-get install -y cmake device-tree-compiler libfdt-dev
+curl -L "https://gitlab.rylanswebsite.com/rylan-meilutis/rppal/-/archive/main/rppal-main.tar.gz" | tar xz
+sed -i 's#rppal = { git = "https://gitlab.rylanswebsite.com/rylan-meilutis/rppal.git", branch = "main" }#rppal = { path = "rppal-main" }#' Cargo.toml
 tox
 TWINE_PASSWORD=${CI_JOB_TOKEN} TWINE_USERNAME=gitlab-ci-token twine upload --repository-url "${CI_API_V4_URL}"/projects/"${CI_PROJECT_ID}"/packages/pypi target/wheels/*
 api_token=$PYPI_TOKEN
